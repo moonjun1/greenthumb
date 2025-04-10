37a892fa69c279099e5b1a4abe79315e5a5069bb
moonjun1 <puls55555@naver.com>
2025-04-10 15:57:44 +0900

    Initial settings

---
+++ src/main/java/com/greenthumb/greenthumb/GreenthumbApplication.java
@@ -0,0 +1,13 @@
+package com.greenthumb.greenthumb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenthumbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenthumbApplication.class, args);
+    }
+
+}
